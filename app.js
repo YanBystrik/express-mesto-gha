374b8177c73b165d9affd64d314625fe59e01092
Yan <yanbystrikov@ya.ru>
2022-03-12 22:25:40 +0300

    обработка не правильного пути

--- app.js
+++ app.js
@@ -29,3 +29,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(require('./routes/users'));
 app.use(require('./routes/cards'));
+
+app.use((req, res, next) => {
+  res.status(404).send({ message: '404 такой страницы нет' });
+
+  next();
+});
