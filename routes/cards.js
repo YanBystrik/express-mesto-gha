377f790de097f8f1329b4fac3e3eca1a4b207ba5
Yan <yanbystrikov@ya.ru>
2022-06-12 17:01:58 +0300

    длина имени карточки

--- routes/cards.js
+++ routes/cards.js
@@ -8,7 +8,7 @@ router.get('/cards', getCards);
 
 router.post('/cards', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(3),
+    name: Joi.string().required().min(2).max(30),
     link: Joi.string().required().pattern(/[-a-zA-Z0-9@:%_\\+.~#?&\\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\\+.~#?&\\/=]*)?/i),
   }),
 }), createCard);
