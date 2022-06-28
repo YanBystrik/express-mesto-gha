dd2ae795e6457c2eeb6103ecf8cd815b0e60ce67
Yan <yanbystrikov@ya.ru>
2022-06-28 20:25:23 +0300

    fuckigsad cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   const origin = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', allowedCors);
     const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
     if (method === 'OPTIONS') {
       const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
@@ -18,7 +18,6 @@ module.exports = (req, res, next) => {
       res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
       // разрешаем кросс-доменные запросы с этими заголовками
       res.header('Access-Control-Allow-Headers', requestHeaders);
-      res.header('Access-Control-Allow-Origin', origin);
 
       return res.end();
     }
